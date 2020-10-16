40ac7709b3c1f233cdf053bcbc417a318da47645
jawale204 <jawale204@gmail.com>
2020-10-16 23:24:44 +0530

    scroll when tapped working

--- demoapp/lib/task2.dart
+++ demoapp/lib/task2.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 
 class Task2 extends StatefulWidget {
@@ -15,6 +14,10 @@ class _Task2State extends State<Task2> {
   var selectedpriority;
   var optionValue1;
   var optionValue2;
+//   final ItemScrollController itemScrollController = ItemScrollController();
+// final ItemPositionsListener itemPositionsListener = ItemPositionsListener.create();
+// final ItemScrollController itemScrollController1 = ItemScrollController();
+// final ItemPositionsListener itemPositionsListener1 = ItemPositionsListener.create();
   var json = """
 [
     {"Management committee":{
@@ -53,31 +56,29 @@ class _Task2State extends State<Task2> {
   void initState() {
     _scrollController1 = ScrollController();
     _scrollController2 = ScrollController();
-    _scrollController1.addListener(listen1);
-    _scrollController2.addListener(listen2);
+    // _scrollController1.addListener(listen1);
+    // _scrollController2.addListener(listen2);
     super.initState();
-     problems = [];
+    problems = [];
   }
 
-  listen1() {
-    var maxscrollvalue = _scrollController1.position.maxScrollExtent;
-    var div = (maxscrollvalue / problems.length) + 10;
-    var scrollvalue = _scrollController2.position.pixels;
-    optionValue1 = (scrollvalue / div).round();
-    print(optionValue1);
-  }
+  // listen1() {
+  //   var maxscrollvalue = _scrollController1.position.maxScrollExtent;
+  //   var div = (maxscrollvalue / problems.length) + 10;
+  //   var scrollvalue = _scrollController2.position.pixels;
+  //   optionValue1 = (scrollvalue / div).round();
 
-  listen2() {
-    var maxscrollvalue = _scrollController2.position.maxScrollExtent;
-    var div = (maxscrollvalue / problems.length) + 10;
-    var scrollvalue = _scrollController2.position.pixels;
-    optionValue2 = (scrollvalue / div).round();
-    print(optionValue2);
-   
-  }
+  // }
+
+  // listen2() {
+  //   var maxscrollvalue = _scrollController2.position.maxScrollExtent;
+  //   var div = (maxscrollvalue / problems.length) + 10;
+  //   var scrollvalue = _scrollController2.position.pixels;
+  //   optionValue2 = (scrollvalue / div).round();
+  // }
 
   perform() {
-     problems = [];
+    problems = [];
     var jsondecode = jsonDecode(json);
     for (Map<String, dynamic> b in jsondecode) {
       b.forEach((key, value) {
@@ -87,6 +88,26 @@ class _Task2State extends State<Task2> {
         });
         problems.add([key, use]);
       });
+    }
+  }
+
+  changeposition(which, index) {
+    print(_scrollController1.position.maxScrollExtent / problems.length);
+    print(_scrollController2.position.maxScrollExtent / problems.length);
+    if (which == 1) {
+      _scrollController2.animateTo(
+          ((_scrollController1.position.maxScrollExtent / problems.length) +
+                  10) *
+              index,
+          curve: Curves.easeIn,
+          duration: Duration(milliseconds: 500));
+    } else {
+      _scrollController1.animateTo(
+          ((_scrollController1.position.maxScrollExtent / problems.length) +
+                  15) *
+              index,
+          curve: Curves.easeIn,
+          duration: Duration(milliseconds: 500));
     }
   }
 
@@ -125,20 +146,25 @@ class _Task2State extends State<Task2> {
               scrollDirection: Axis.horizontal,
               itemCount: problems.length,
               itemBuilder: (context, index) {
-                return Container(
-                    padding: EdgeInsets.all(0),
-                    child: Card(
-                        color: Colors.blue[900],
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(3.0),
-                            child: Text(
-                              problems[index][0],
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.white),
+                return GestureDetector(
+                  onTap: () {
+                    changeposition(1, index);
+                  },
+                  child: Container(
+                      padding: EdgeInsets.all(0),
+                      child: Card(
+                          color: Colors.blue[900],
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: Text(
+                                problems[index][0],
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.white),
+                              ),
                             ),
-                          ),
-                        )));
+                          ))),
+                );
               },
             ),
           ),
@@ -156,46 +182,52 @@ class _Task2State extends State<Task2> {
                     scrollDirection: Axis.vertical,
                     itemCount: problems.length,
                     itemBuilder: (context, index1) {
-                      return Column(children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  color: Colors.grey[300],
-                                ),
-                                padding: EdgeInsets.all(5),
-                                child: Text(
-                                  problems[index1][0],
-                                  textAlign: TextAlign.left,
-                                  style: TextStyle(
-                                      fontSize: 20, color: Colors.blue[600]),
-                                ))
-                          ],
-                        ),
-                        Divider(
-                          height: 1,
-                        ),
-                        ListView.builder(
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: problems[index1][1].length,
-                            itemBuilder: (context, index2) {
-                              return Container(
-                                  margin: EdgeInsets.all(3.0),
+                      return GestureDetector(
+                        onTap: () {
+                          changeposition(2, index1);
+                        },
+                        child: Column(children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
                                   decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      color: Colors.white),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(4.0),
-                                    child: Text(
-                                      problems[index1][1][index2],
-                                      style: TextStyle(fontSize: 15),
-                                    ),
-                                  ));
-                            }),
-                      ]);
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.grey[300],
+                                  ),
+                                  padding: EdgeInsets.all(5),
+                                  child: Text(
+                                    problems[index1][0],
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.blue[600]),
+                                  ))
+                            ],
+                          ),
+                          Divider(
+                            height: 1,
+                          ),
+                          ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: problems[index1][1].length,
+                              itemBuilder: (context, index2) {
+                                return Container(
+                                    margin: EdgeInsets.all(3.0),
+                                    decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        color: Colors.white),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: Text(
+                                        problems[index1][1][index2],
+                                        style: TextStyle(fontSize: 15),
+                                      ),
+                                    ));
+                              }),
+                        ]),
+                      );
                     },
                   ),
                 )),
